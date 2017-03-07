3aa3d6d84c8d81cfb04be43900f4896066689411
zhiwen <zhangzhiwen143@gmail.com>
2017-03-07 13:11:27 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/zhiwen143/coolweather/db/County.java
@@ -0,0 +1,16 @@
+package com.zhiwen143.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by msi on 2017/3/7.
+ */
+
+public class County extends DataSupport {
+	
+	private int id;
+	private String countyName;
+	private String weatherId;
+	private int cityId;
+	
+}
